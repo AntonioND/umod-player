AntonioND/umod-player

commit 0654fff14f4496558c104a19f1fad47bab771aea

packer: player: Implement MOD Porta + Vol Slide (5xy)

player/source/mod_channel.c
@@ -334,6 +334,8 @@ void ModChannelUpdateAllTick(int tick_number)
                 ch->effect = EFFECT_NONE;
                 MixerChannelSetVolume(handle, 0);
             }
+
+            return;
         }
         else if (ch->effect == EFFECT_ARPEGGIO)
         {
@@ -360,25 +362,8 @@ void ModChannelUpdateAllTick(int tick_number)
 
             uint32_t amiga_period = ModNoteToAmigaPeriod(note, 0);
             ch->amiga_period = amiga_period;
-        }
-        else if (ch->effect == EFFECT_VOLUME_SLIDE)
-        {
-            if (tick_number > 0)
-            {
-                int volume = ch->volume + (int8_t)ch->effect_params;
 
-                if (volume > 255)
-                    volume = 255;
-
-                if (volume < 0)
-                    volume = 0;
-
-                if (volume != ch->volume)
-                {
-                    ch->volume = volume;
-                    MixerChannelSetVolume(handle, volume);
-                }
-            }
+            return;
         }
         else if (ch->effect == EFFECT_FINE_VOLUME_SLIDE)
         {
@@ -398,6 +383,8 @@ void ModChannelUpdateAllTick(int tick_number)
                     MixerChannelSetVolume(handle, volume);
                 }
             }
+
+            return;
         }
         else if (ch->effect == EFFECT_PORTA_UP)
         {
@@ -411,6 +398,8 @@ void ModChannelUpdateAllTick(int tick_number)
                 period = ModGetSampleTickPeriodFromAmigaPeriod(ch->amiga_period);
                 MixerChannelSetNotePeriod(ch->mixer_channel_handle, period);
             }
+
+            return;
         }
         else if (ch->effect == EFFECT_FINE_PORTA_UP)
         {
@@ -424,6 +413,8 @@ void ModChannelUpdateAllTick(int tick_number)
                 period = ModGetSampleTickPeriodFromAmigaPeriod(ch->amiga_period);
                 MixerChannelSetNotePeriod(ch->mixer_channel_handle, period);
             }
+
+            return;
         }
         else if (ch->effect == EFFECT_PORTA_DOWN)
         {
@@ -435,6 +426,8 @@ void ModChannelUpdateAllTick(int tick_number)
                 period = ModGetSampleTickPeriodFromAmigaPeriod(ch->amiga_period);
                 MixerChannelSetNotePeriod(ch->mixer_channel_handle, period);
             }
+
+            return;
         }
         else if (ch->effect == EFFECT_FINE_PORTA_DOWN)
         {
@@ -446,6 +439,8 @@ void ModChannelUpdateAllTick(int tick_number)
                 period = ModGetSampleTickPeriodFromAmigaPeriod(ch->amiga_period);
                 MixerChannelSetNotePeriod(ch->mixer_channel_handle, period);
             }
+
+            return;
         }
         else if (ch->effect == EFFECT_SAMPLE_OFFSET)
         {
@@ -466,8 +461,33 @@ void ModChannelUpdateAllTick(int tick_number)
 
                 MixerChannelSetSampleOffset(handle, offset);
             }
+
+            return;
         }
-        else if (ch->effect == EFFECT_PORTA_TO_NOTE)
+
+        if ((ch->effect == EFFECT_VOLUME_SLIDE) ||
+            (ch->effect == EFFECT_PORTA_VOL_SLIDE))
+        {
+            if (tick_number > 0)
+            {
+                int volume = ch->volume + (int8_t)ch->effect_params;
+
+                if (volume > 255)
+                    volume = 255;
+
+                if (volume < 0)
+                    volume = 0;
+
+                if (volume != ch->volume)
+                {
+                    ch->volume = volume;
+                    MixerChannelSetVolume(handle, volume);
+                }
+            }
+        }
+
+        if ((ch->effect == EFFECT_PORTA_TO_NOTE) ||
+            (ch->effect == EFFECT_PORTA_VOL_SLIDE))
         {
             if (tick_number != 0)
             {

packer/mod.c
@@ -318,6 +318,27 @@ int add_mod(const char *path)
                         converted_effect_param = effect_params;
                         break;
                     }
+                    case 0x5: // Porta + Volume slide
+                    {
+                        converted_effect = EFFECT_PORTA_TO_NOTE;
+                        converted_effect_param = 0;
+
+                        int dec = effect_params & 0xF;
+                        int inc = effect_params >> 4;
+
+                        // If both are set, ignore effect
+                        if ((dec > 0) && (inc > 0))
+                            break;
+
+                        converted_effect = EFFECT_PORTA_VOL_SLIDE;
+
+                        if (inc > 0)
+                            converted_effect_param = inc * MOD_VOLUME_SCALE;
+                        if (dec > 0)
+                            converted_effect_param = -dec * MOD_VOLUME_SCALE;
+
+                        break;
+                    }
                     case 0x8: // Pan
                     {
                         // 0x00 = left, 0xFF = right
@@ -464,7 +485,6 @@ int add_mod(const char *path)
                         break;
                     }
                     case 0x4: // Vibrato
-                    case 0x5: // Porta + Volume slide
                     case 0x6: // Vibrato + Volume slide
                     case 0x7: // Tremolo
                     default:

player/include/umod/umodpack.h
@@ -66,6 +66,7 @@ typedef struct {
 #define EFFECT_FINE_VOLUME_SLIDE    12
 #define EFFECT_SAMPLE_OFFSET        13
 #define EFFECT_PORTA_TO_NOTE        14
+#define EFFECT_PORTA_VOL_SLIDE      15
 // TODO: Reorder effect numbers
 
 #endif // UMOD_UMODPACK_H__

player/source/player.c
@@ -256,8 +256,11 @@ static void UMOD_Tick(void)
 
         if (note != -1)
         {
-            if (effect != EFFECT_PORTA_TO_NOTE)
+            if ((effect != EFFECT_PORTA_TO_NOTE) &&
+                (effect != EFFECT_PORTA_VOL_SLIDE))
+            {
                 ModChannelSetNote(c, note);
+            }
         }
 
         if (volume != -1)

commit 4d6a2aecc4c9b777a40535a16777a115be42ae02

player: Fix multiple effects in one row not working
In some cases, when multiple channels have effects in the same row, the
last ones would be ignored. This was happening because of some returns
in the modified function by this patch, which should have been continue.
It didn't happen with all effects, which made it harder to detect.

This bug was introduced in commit 0654fff14f44 ("packer: player:
Implement MOD Porta + Vol Slide (5xy)").

player/source/mod_channel.c
@@ -350,7 +350,7 @@ void ModChannelUpdateAllTick(int tick_number)
                 MixerChannelSetVolume(handle, 0);
             }
 
-            return;
+            continue;
         }
         else if (ch->effect == EFFECT_ARPEGGIO)
         {
@@ -378,7 +378,7 @@ void ModChannelUpdateAllTick(int tick_number)
             uint32_t amiga_period = ModNoteToAmigaPeriod(note, 0);
             ch->amiga_period = amiga_period;
 
-            return;
+            continue;
         }
         else if (ch->effect == EFFECT_FINE_VOLUME_SLIDE)
         {
@@ -399,7 +399,7 @@ void ModChannelUpdateAllTick(int tick_number)
                 }
             }
 
-            return;
+            continue;
         }
         else if (ch->effect == EFFECT_PORTA_UP)
         {
@@ -414,7 +414,7 @@ void ModChannelUpdateAllTick(int tick_number)
                 MixerChannelSetNotePeriod(ch->mixer_channel_handle, period);
             }
 
-            return;
+            continue;
         }
         else if (ch->effect == EFFECT_FINE_PORTA_UP)
         {
@@ -429,7 +429,7 @@ void ModChannelUpdateAllTick(int tick_number)
                 MixerChannelSetNotePeriod(ch->mixer_channel_handle, period);
             }
 
-            return;
+            continue;
         }
         else if (ch->effect == EFFECT_PORTA_DOWN)
         {
@@ -442,7 +442,7 @@ void ModChannelUpdateAllTick(int tick_number)
                 MixerChannelSetNotePeriod(ch->mixer_channel_handle, period);
             }
 
-            return;
+            continue;
         }
         else if (ch->effect == EFFECT_FINE_PORTA_DOWN)
         {
@@ -455,7 +455,7 @@ void ModChannelUpdateAllTick(int tick_number)
                 MixerChannelSetNotePeriod(ch->mixer_channel_handle, period);
             }
 
-            return;
+            continue;
         }
         else if (ch->effect == EFFECT_SAMPLE_OFFSET)
         {
@@ -477,7 +477,7 @@ void ModChannelUpdateAllTick(int tick_number)
                 MixerChannelSetSampleOffset(handle, offset);
             }
 
-            return;
+            continue;
         }
 
         if ((ch->effect == EFFECT_VIBRATO) ||
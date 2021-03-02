13abccdda4ffd008980f775861509919e2866538
Antonio Niño Díaz <antonio_nd@outlook.com>
2021-03-02 21:22:39 +0000

    tests: Add first SFX test

--- tests/cmake/test.cmake
+++ tests/cmake/test.cmake
@@ -50,3 +50,80 @@ function(test_mod_wav mod_name)
     add_dependencies(generate_references ${base_name}_mod_test_generate)
 
 endfunction()
+
+
+function(test_sfx_wav)
+
+    # Generate file names
+
+    get_filename_component(directory_name ${CMAKE_CURRENT_SOURCE_DIR} NAME_WE)
+
+    set(BINARY_NAME "test_sfx_${directory_name}")
+
+    set(REF_TAR_BZ "${CMAKE_CURRENT_SOURCE_DIR}/reference.wav.tar.bz")
+
+    set(REF_WAV "${CMAKE_CURRENT_BINARY_DIR}/reference.wav")
+    set(REF_PACK "${CMAKE_CURRENT_BINARY_DIR}/pack.bin")
+    set(REF_HEADER "${CMAKE_CURRENT_BINARY_DIR}/pack_header.h")
+    set(GEN_WAV "${CMAKE_CURRENT_BINARY_DIR}/generated.wav")
+
+    # Build source files
+
+    add_executable(${BINARY_NAME})
+
+    search_source_files(. FILES_SOURCE)
+
+    target_sources(${BINARY_NAME} PRIVATE ${FILES_SOURCE})
+
+    target_link_libraries(${BINARY_NAME} umod_player utils)
+
+    # Create pack file
+
+    search_audio_files(. FILES_AUDIO)
+
+    add_custom_command(
+        OUTPUT ${REF_PACK} ${REF_HEADER}
+        COMMAND $<TARGET_FILE:umod_packer> ${REF_PACK} ${REF_HEADER} ${FILES_AUDIO}
+        DEPENDS ${FILES_AUDIO}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+    )
+
+    # Add output source files to the target
+
+    message(VERBOSE "${directory_name}: Adding ${FILES_AUDIO}")
+
+    target_sources(${BINARY_NAME} PRIVATE ${REF_HEADER})
+    target_include_directories(${BINARY_NAME} PRIVATE ${CMAKE_CURRENT_BINARY_DIR})
+    set_source_files_properties(${REF_PACK} ${REF_HEADER} PROPERTIES GENERATED 1)
+
+    # Add test to CTest
+
+    set(CMD1 "${CMAKE_COMMAND} -E tar -xf ${REF_TAR_BZ}")
+    set(CMD2 "$<TARGET_FILE:${BINARY_NAME}> ${GEN_WAV}")
+    set(CMD3 "${CMAKE_COMMAND} -E compare_files ${REF_WAV} ${GEN_WAV}")
+
+    add_test(NAME ${directory_name}_sfx_test
+        COMMAND ${CMAKE_COMMAND}
+                    -DCMD1=${CMD1}
+                    -DCMD2=${CMD2}
+                    -DCMD3=${CMD3}
+                    -P ${CMAKE_SOURCE_DIR}/tests/cmake/runcommands.cmake
+        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+    )
+
+    # Add target to autogenerate the new compressed file to be used as reference
+
+    set(CMD1 "$<TARGET_FILE:${BINARY_NAME}> ${REF_WAV}")
+    set(CMD2 "${CMAKE_COMMAND} -E tar -cfj ${REF_TAR_BZ} ${REF_WAV}")
+
+    add_custom_target(${directory_name}_sfx_test_generate
+        COMMAND ${CMAKE_COMMAND}
+                    -DCMD1=${CMD1}
+                    -DCMD2=${CMD2}
+                    -P ${CMAKE_SOURCE_DIR}/tests/cmake/runcommands.cmake
+        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+    )
+
+    add_dependencies(generate_references ${directory_name}_sfx_test_generate)
+
+endfunction()
---
+++ tests/sfx/basic/main.c
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: MIT
+//
+// Copyright (c) 2021 Antonio Niño Díaz
+
+// Test a simple oneshot SFX and a looping one.
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <umod/umod.h>
+
+#include "file.h"
+#include "wav_utils.h"
+
+#include "pack_header.h"
+
+#define SAMPLE_RATE (32 * 1024)
+
+void generate_ms(int ms)
+{
+    for (int t = 0; t < ms; t++)
+    {
+#define SIZE (SAMPLE_RATE / 1000)
+
+        int8_t left[SIZE], right[SIZE];
+        UMOD_Mix(&left[0], &right[0], SIZE);
+
+        uint8_t buffer[SIZE * 2];
+        for (int i = 0; i < SIZE; i++)
+        {
+            buffer[i * 2 + 0] = left[i] + 128;
+            buffer[i * 2 + 1] = right[i] + 128;
+        }
+
+        WAV_FileStream(buffer, sizeof(buffer));
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("Invalid number of arguments\n");
+        return -1;
+    }
+
+    // Load file
+
+    void *pack_buffer = NULL;
+    size_t pack_size;
+
+    file_load("pack.bin", &pack_buffer, &pack_size);
+    if (pack_size == 0)
+        goto cleanup;
+
+    // Play music until the song ends, while saving it to a WAV
+
+    UMOD_Init(SAMPLE_RATE);
+
+    int ret = UMOD_LoadPack(pack_buffer);
+    if (ret != 0)
+    {
+        printf("UMOD_LoadPack() failed\n");
+        goto cleanup;
+    }
+
+    WAV_FileStart(argv[1], SAMPLE_RATE);
+    if (!WAV_FileIsOpen())
+        goto cleanup;
+
+    UMOD_SFX_Play(SFX_LASER2_1_WAV, UMOD_LOOP_DEFAULT);
+
+    generate_ms(2000);
+
+    UMOD_SFX_Play(SFX_HELICOPTER_WAV, UMOD_LOOP_ENABLE);
+
+    generate_ms(4000);
+
+    WAV_FileEnd();
+
+cleanup:
+    free(pack_buffer);
+    return 0;
+}
